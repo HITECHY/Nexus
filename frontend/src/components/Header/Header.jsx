695749b51a22bcada34aecac6551b0a4bcf636a6
HITESH KUMAR <91737883+HITECHY@users.noreply.github.com>
2024-09-15 02:51:54 +0530

    Add files via upload

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='Header'>
+      <div className="Header-contents">
+        <h2>Order your Favourite Food Here</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest culinary expertise. Our mission is to satisfy your needs.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
